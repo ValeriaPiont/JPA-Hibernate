172406720e6f3a798152a9fb07491d5b2d1f4e3a
Valeria Piont <iceflame.vvv@gmail.com>
2022-02-24 12:11:23 +0300

    lection14: resolved Authors & Books task

--- author-book/src/main/java/com/bobocode/model/Book.java
+++ author-book/src/main/java/com/bobocode/model/Book.java
@@ -8,26 +8,9 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-/**
- * todo:
- * - implement no arguments constructor
- * - implement getters and setters for all fields
- * - implement equals() and hashCode() based on {@link Book#isbn}
- * - make setter for field {@link Book#authors} private
- * - initialize field {@link Book#authors} as new {@link HashSet}
- * <p>
- * - configure JPA entity
- * - specify table name: "book"
- * - configure auto generated identifier
- * - configure mandatory column "name" for field {@link Book#name}
- * - configure mandatory unique column "isbn" for field {@link Book#isbn}, it is a natural key candidate
- * <p>
- * - configure many-to-many relation as mapped on the {@link Author} side
- */
 @NoArgsConstructor
 @Getter
 @Setter
-@ToString
 @Table(name = "book")
 @Entity
 public class Book {
@@ -42,12 +25,9 @@ public class Book {
     @Column(nullable = false, unique = true)
     private String isbn;
 
-    @ManyToMany
+    @ManyToMany(mappedBy = "books")
+    @Setter(value = AccessLevel.PRIVATE)
     private Set<Author> authors =  new HashSet<>();
-
-    private void setAuthors(Set<Author> authors) {
-        this.authors = authors;
-    }
 
     @Override
     public boolean equals(Object o) {
