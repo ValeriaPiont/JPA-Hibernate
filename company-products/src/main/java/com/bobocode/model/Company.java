107145973837e349f46851ced15c2b32e67ab902
Valeria Piont <iceflame.vvv@gmail.com>
2022-02-24 14:43:59 +0300

    lection13: resolved company product task

--- company-products/src/main/java/com/bobocode/model/Company.java
+++ company-products/src/main/java/com/bobocode/model/Company.java
@@ -7,23 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-/**
- * todo:
- * - implement no arguments constructor
- * - implement getters and setters for all fields
- * - implement equals() and hashCode() based on identifier field
- * - make setter for field {@link Company#products} private
- * - initialize field {@link Company#products} as new {@link ArrayList}
- * - implement a helper {@link Company#addProduct(Product)} that establishes a relation on both sides
- * - implement a helper {@link Company#removeProduct(Product)} that drops a relation on both sides
- * <p>
- * - configure JPA entity
- * - specify table name: "company"
- * - configure auto generated identifier
- * - configure mandatory column "name" for field {@link Company#name}
- * <p>
- * - configure one to many relationship as mapped on the child side
- */
+
 @NoArgsConstructor
 @Getter
 @Setter
--- company-products/src/main/java/com/bobocode/model/Product.java
+++ company-products/src/main/java/com/bobocode/model/Product.java
@@ -1,6 +1,5 @@
 package com.bobocode.model;
 
-import jdk.jfr.Enabled;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,20 +7,6 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.util.Objects;
 
-/**
- * todo:
- * - implement no arguments constructor
- * - implement getters and setters for all fields
- * - implement equals() and hashCode() based on identifier field
- * <p>
- * - configure JPA entity
- * - specify table name: "product"
- * - configure auto generated identifier
- * - configure mandatory column "name" for field {@link Product#name}
- * <p>
- * - configure lazy many-to-one relation between {@link Product} and {@link Company}
- * - configure foreign key column "company_id" references company table
- */
 @NoArgsConstructor
 @Getter
 @Setter
