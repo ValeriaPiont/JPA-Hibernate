4e7121a1bdae075fba57409274be35b9c7cc752c
Valeria Piont <iceflame.vvv@gmail.com>
2022-02-25 13:07:43 +0300

    lection13: resolved Photo comment DAO task

--- photo-comment-dao/src/main/java/com/bobocode/model/PhotoComment.java
+++ photo-comment-dao/src/main/java/com/bobocode/model/PhotoComment.java
@@ -1,29 +1,43 @@
 package com.bobocode.model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-/**
- * todo:
- * - implement no argument constructor
- * - implement getters and setters
- * - implement equals and hashCode based on identifier field
- *
- * - configure JPA entity
- * - specify table name: "photo_comment"
- * - configure auto generated identifier
- * - configure not nullable column: text
- *
- * - map relation between Photo and PhotoComment using foreign_key column: "photo_id"
- * - configure relation as mandatory (not optional)
- */
+@NoArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "photo_comment")
 public class PhotoComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(nullable = false)
     private String text;
+
     private LocalDateTime createdOn;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "photo_id")
     private Photo photo;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PhotoComment that = (PhotoComment) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
